49928da87343492fbdb9df019e4ee58a67232316
Sersoc <mysky213@gmail.com>
2020-01-10 10:50:06 +0900

    B-11654 added

---
+++ b11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class b11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a = sc.nextLine().charAt(0);
+		int b=(int)a;
+		System.out.print(b);
+	}
+
+}
