4c590688f393426fe21652a5112889e47b7cdf1f
Sersoc <mysky213@gmail.com>
2020-01-16 15:35:28 +0900

    B-1152 added

---
+++ b1152.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class b1152 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine().trim();
+		String[] gon = s.split(" ");
+		System.out.println(gon.length);
+		
+		
+	}
+
+}
